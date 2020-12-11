4172d5d0cbcfe3d67701b713ea4b30a1872ff378
cvergararetamal <chrvergararetamal@gmail.com>
2020-12-11 02:16:43 -0300

    Estructura básica

--- flota/models/camion.py
+++ flota/models/camion.py
@@ -4,12 +4,13 @@ from odoo import models, fields, api
 
 class marca(models.Model):
     _name = 'flota.marca'
+    _rec_name = 'marca'
     
     marca = fields.Char(string="Marca",required=True) 
     camiones_ids = fields.One2many(
         'flota.camion',
         'marca_id',
-        string="MArcas")
+        string="Marcas")
     
     total_camiones = fields.Integer(string="Total de camiones", compute="_total_camiones")
     
@@ -42,10 +43,10 @@ class camion(models.Model):
     marca_id = fields.Many2one('flota.marca',string='Marca')
     modelo_id = fields.Many2one('flota.modelo',string='Modelo')
     patente = fields.Char(string='Patente', required=True)
-    chasis = fields.Integer(string='Chasis')
-    color = fields.Char(string='Color', required=True)
-    cilindrada = fields.Float(string='Cilindrada')
-    autonomia = fields.Float(string='Autonomía')
+    color = fields.Char(string='Color')
+    cilindrada = fields.Float(string='Cilindrada (cc)')
+    autonomia = fields.Float(string='Autonomía (kms)')
+    chasis = fields.Integer(string='Número de chasis', required=True)
     año = fields.Integer(string='Año')
     kilometraje = fields.Integer(string='Kilometraje', required=True)
     
